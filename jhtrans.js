506c881b259df6237fd3d001d11b95809dbd8f33
mitsuaki kuwahara <quwahara@gmail.com>
2019-11-04 17:04:28 +0900

    Add translate method

--- jhtrans.js
+++ jhtrans.js
@@ -29,24 +29,92 @@
       return typeof v === "string";
     }
 
-    var Jhtrans = function Jhtrans(templates) {
+    var Jhtrans = function Jhtrans() {
       // Dictionary for templates
       this.templates = {};
+      this.config = {
+        RECURSION_MAX: 1000
+      };
     };
 
     Jhtrans.prototype.getTemplate = function (name) {
 
       if (this.templates.hasOwnProperty(name)) {
-        return this.templates[name];
+        return this.templates[name].cloneNode(true);
       }
 
       throw Error("template not found for: '" + name + "'");
     };
 
+    Jhtrans.prototype.translate = function (name, map) {
+      return this.translateElement(
+        this.getTemplate(name),
+        map
+      );
+    };
+
+    Jhtrans.prototype.translateElement = function (elm, map, recursion) {
+
+      map = map || {};
+      recursion = recursion || 0;
+
+      if (recursion > this.config.RECURSION_MAX) {
+        throw Error("recursion was over max");
+      }
+
+      var cns = elm.childNodes;
+      var cn;
+      var i;
+      var text;
+      var name;
+      var tmpl;
+      var trans;
+      for (i = 0; i < cns.length; ++i) {
+        cn = cns.item(i);
+        if (cn.nodeType === Node.ELEMENT_NODE) {
+          this.translateElement(cn, map, recursion + 1);
+        }
+        else if (cn.nodeType === Node.TEXT_NODE) {
+          name = null;
+          text = (cn.textContent || "").trim();
+
+          if (text.length >= 3 && text.indexOf("~@") === 0) {
+            // replace by holder name with map
+            if (!map.hasOwnProperty(text.substr(2))) {
+              throw Error("map not found for: '" + text.substr(2) + "'");
+            }
+            name = map[text.substr(2)];
+          }
+          else if (text.length >= 3 && text.indexOf("~#") === 0) {
+            // replace by template name
+            name = text.substr(2);
+          }
+
+          if (name !== null) {
+            tmpl = this.getTemplate(name);
+            trans = this.translateElement(tmpl, map, recursion + 1);
+            elm.replaceChild(trans, cn);
+          }
+        }
+      }
+
+      return elm;
+    };
+
     Jhtrans.prototype.putTemplate = function (name, definition) {
       var template = this.definitionToElement(definition);
       this.templates[name] = template;
-      return template;
+      return this;
+    };
+
+    Jhtrans.prototype.putTemplateAll = function (nameDefinitions) {
+      for (var name in nameDefinitions) {
+        if (!Object.prototype.hasOwnProperty.call(nameDefinitions, name)) {
+          continue;
+        }
+        this.putTemplate(name, nameDefinitions[name]);
+      }
+      return this;
     };
 
     Jhtrans.prototype.definitionToElement = function (definition) {
