efd965fe91ad62199a69e8490b09cf4654133348
Mitsuaki Kuwahara <quwahara@gmail.com>
2019-12-10 20:10:01 +0900

    Remove coment out

--- libs/jhtrans.js
+++ libs/jhtrans.js
@@ -69,46 +69,6 @@
     return copies;
   }
 
-  const Context = function Context(parentContext) {
-    this.parentContext = parentContext;
-    this.depth = 0;
-    this.pl = "";
-    this.recursion = 0;
-    this.index = 0;
-  }
-
-  Context.prototype.incRecursion = function () {
-    this.recursion += 1;
-  };
-
-  Context.prototype.decRecursion = function () {
-    this.recursion -= 1;
-  };
-
-  Context.prototype.resetIndex = function () {
-    this.index = 0;
-  };
-
-  Context.prototype.incIndex = function () {
-    this.index += 1;
-  };
-
-  Context.prototype.sink = function () {
-    const deeper = new Context(this.parentContext);
-    deeper.depth = this.depth + 1;
-    deeper.pl = this.getPlKey();
-    deeper.recursion = this.recursion;
-    deeper.index = this.index;
-    return deeper;
-  }
-
-  Context.prototype.getPlKey = function () {
-    const indexStr = isNullOrUndefined(this.index) ? "" : "x" + this.index;
-    const plh = this.pl + (this.pl ? "-" : "");
-    const plKey = plh + this.depth + indexStr;
-    return plKey;
-  }
-
   const Jhtrans = function Jhtrans() {
     // Dictionary for templates
     this.templates = {};
@@ -228,7 +188,6 @@
     return null;
   }
 
-
   Jhtrans.prototype.translate = function (key, replacements) {
 
     let elementNode;
@@ -276,17 +235,17 @@
 
     let lastNode = textNode;
     for (let i = 0; i < replacements.length; ++i) {
-      lastNode = this.processTextNodeBy2(textNodes[i], replacements[i]);
+      lastNode = this.processReplacement(textNodes[i], replacements[i]);
     }
     return elementNode;
   }
 
-  Jhtrans.prototype.processTextNodeBy2 = function (textNode, replacement) {
+  Jhtrans.prototype.processReplacement = function (textNode, replacement) {
 
-    console.log("processTextNodeBy", { "textNode": textNode, "replacement": replacement });
+    console.log("processReplacement", { "textNode": textNode, "replacement": replacement });
 
     if (isString(replacement)) {
-      return this.replaceString2(textNode, replacement);
+      return this.processString(textNode, replacement);
     }
     else if (isObject(replacement)) {
       if (!replacement.hasOwnProperty("k")) {
@@ -307,119 +266,15 @@
     }
   };
 
-  Jhtrans.prototype.replaceString2 = function (textNode, string) {
+  Jhtrans.prototype.processString = function (textNode, string) {
+
+    console.log("processString", { "textNode": textNode, "string": string });
+
     if (isTemplateKey(string)) {
       const templ = this.getTemplate(string.substring(1));
       if (!isNullOrUndefined(templ)) {
         textNode.parentNode.replaceChild(templ, textNode);
         return templ;
-      }
-    }
-    textNode.textContent = string;
-    return textNode;
-  };
-
-
-
-
-  Jhtrans.prototype.processNode = function (node, replacements) {
-
-    let outNode;
-
-    if (isNullOrUndefined(replacements) || replacements.length === 0) {
-      return node;
-    }
-    else if (isElementNode(node)) {
-      outNode = this.processElementNode(node, replacements);
-    }
-    else if (isTextNode(node)) {
-      outNode = this.processTextNode(node, replacements);
-    } else {
-      outNode = node;
-    }
-
-    return outNode;
-  };
-
-  Jhtrans.prototype.processElementNode = function (elementNode, replacements) {
-
-    const cns = copyChildNodes(elementNode.childNodes);
-
-    console.log("processElementNode", { "elementNode": elementNode, "childNodes": cns, "replacements": replacements });
-
-    for (let i = 0; i < cns.length; ++i) {
-      this.processNode(cns[i], replacements);
-    }
-
-    return elementNode;
-  };
-
-  Jhtrans.prototype.processTextNode = function (textNode, replacements) {
-
-    const text = textNode.textContent.trim();
-
-    if (!isPlaceholderKey(text)) {
-      return textNode;
-    }
-
-    const replacement = replacements.shift();
-
-    return this.processTextNodeBy(textNode, replacement, replacements);
-  };
-
-  Jhtrans.prototype.processTextNodeBy = function (textNode, replacement, replacements) {
-
-    console.log("processTextNodeBy", { "textNode": textNode, "replacement": replacement, "replacements": replacements });
-
-    if (isString(replacement)) {
-      return this.replaceString(textNode, replacement, replacements);
-    }
-    else if (isObject(replacement)) {
-      if (!replacement.hasOwnProperty("k")) {
-        throw Error("'k' was not found for the replacement object.");
-      }
-      if (!replacement.hasOwnProperty("rs")) {
-        throw Error("'rs' was not found for the replacement object.");
-      }
-
-      const translated = this.translate(replacement.k, replacement.rs);
-
-      textNode.parentNode.replaceChild(translated, textNode);
-
-      return translated;
-    }
-    else if (isArray(replacement)) {
-      // Justify number of text node to be equivalent with the array length.
-      const textNodes = [];
-      for (let i = 1; i < replacement.length; ++i) {
-        const createdTextNode = document.createTextNode("");
-        textNode.parentNode.insertBefore(createdTextNode, textNode);
-        textNodes.push(createdTextNode);
-      }
-      textNodes.push(textNode);
-
-      let lastNode = textNode;
-      for (let i = 0; i < replacement.length; ++i) {
-        if (isString(replacement[i])) {
-          lastNode = this.processTextNodeBy(textNodes[i], replacement[i], replacements);
-        }
-        else if (isArray(replacement[i])) {
-          lastNode = this.processTextNodeBy(textNodes[i], replacement[i][0], replacement[i][1]);
-        }
-      }
-      return lastNode.parentNode.childNodes;
-    }
-    else {
-      return textNode;
-    }
-  };
-
-  Jhtrans.prototype.replaceString = function (textNode, string, replacements) {
-    if (isTemplateKey(string)) {
-      const templ = this.getTemplate(string.substring(1));
-      if (!isNullOrUndefined(templ)) {
-        textNode.parentNode.replaceChild(templ, textNode);
-        return this.processNode(templ, replacements);
       }
     }
     textNode.textContent = string;
