84f14397ff0b4bc0d1772aa1f6511de08253e15e
Mitsuaki Kuwahara <quwahara@gmail.com>
2020-01-17 18:53:27 +0900

    Change depth of structure

--- labo/login.js
+++ labo/login.js
@@ -6,24 +6,20 @@
 
     jht.setGlobalReplacement("@class", "");
     const contents = jht.translate({
-        "#": "div", "@": [
+        "#": "center-max320", "@class-inner": "pw14", "@": [
+            { "#": "h1", "@": "Log in", "@class": "pse14" },
+            { "#": "labeled", "@label": "Username", "@target": { "#": "input@text", "@name": "username" } },
+            { "#": "labeled", "@label": "Password", "@target": { "#": "input@password", "@name": "password" } },
             {
-                "#": "center-max320", "@class-inner": "pw14", "@": [
-                    { "#": "h1", "@": "Log in", "@class": "pse14" },
-                    { "#": "labeled", "@label": "Username", "@target": { "#": "input@text", "@name": "username" } },
-                    { "#": "labeled", "@label": "Password", "@target": { "#": "input@password", "@name": "password" } },
+                "#": "row", "@": [
                     {
-                        "#": "row", "@": [
-                            {
-                                "#": "col-sm-6", "@class": "pse14", "@":
-                                    { "#": "button@button", "@": "Log in", "@name": "login" }
-                            },
-                            {
-                                "#": "col-sm-6", "@class": "pse14", "@":
-                                    { "#": "button@button", "@": "Cancel", "@name": "cancel" }
-                            },
-                        ]
-                    }
+                        "#": "col-sm-6", "@class": "pse14", "@":
+                            { "#": "button@button", "@": "Log in", "@name": "login" }
+                    },
+                    {
+                        "#": "col-sm-6", "@class": "pse14", "@":
+                            { "#": "button@button", "@": "Cancel", "@name": "cancel" }
+                    },
                 ]
             }
         ]
